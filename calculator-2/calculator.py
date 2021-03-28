6fd8ac9c7eab934a95f8e2639016af4242f16a76
Vira Sukhanova <verusia.s@gmail.com>
2021-03-27 22:15:51 -0500

    completed basic mode

--- calculator-2/calculator.py
+++ calculator-2/calculator.py
@@ -5,3 +5,40 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+while True:
+    user_input = input("Please type an operator and two numbers: ")
+    tokens = user_input.split(" ")
+
+    if tokens[0].lower() == 'q' or tokens[1] == 'q':
+        break
+    else:
+        operator = tokens[0]
+        num1 = tokens[1]
+        num1 = float(num1)
+
+        if len(tokens) < 3:
+            num2 = 0
+        else:
+            num2 = tokens[2]
+            num2 = float(num2)
+
+        result = None
+
+        if operator == "+":
+            result = num1 + num2
+        elif operator == "-":
+            result = num1 - num2
+        elif operator == "*":
+            result = num1 * num2
+        elif operator == "/":
+            result = num1 / num2
+        elif operator == "square":
+            result = num1 ** 2
+        elif operator.lower() == "cube" or operator.lower() == "c":
+            result = num1 ** 3
+        elif operator.lower == "pow" or operator.lower() == "p":
+            result = num1 ** num2
+        elif operator.lower() == "mod" or operator.lower() == "m":
+            result = num1 % num2
+
+        print(result)
