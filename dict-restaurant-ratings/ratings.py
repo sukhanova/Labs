385a7c858c85f58ba4147b231542b7bcdcaef924
Vira Sukhanova <verusia.s@gmail.com>
2021-04-02 17:11:30 -0500

    added function to read file and return dict with restaurant name qnd score

--- dict-restaurant-ratings/ratings.py
+++ dict-restaurant-ratings/ratings.py
@@ -2,13 +2,21 @@
 
 """Read the ratings in from the file, store them in a dictionary and spits out the ratings
  in alphabetical order by restaurant"""
+def process_scores():
+    #  def restaurant_rating(filename):
+    file = open('scores.txt')  
 
-#  def restaurant_rating(filename):
-file = open('scores.txt')  
+    restaurant_scores = {}
 
-restaurant_scores = {}
+    for line in file:
+        line = line.strip()
+        restaurant, score = line.split(":")
+        
+        #prints each line from file in format restaurant: score
+        #print(line)
+        restaurant_scores[restaurant] = int(score)
+    
+    # print(restaurant_scores)
+    return restaurant_scores
 
-for line in file:
-    line = line.strip()
-    restaurant, score = line.split(":")
-    print(line)
+print(process_scores())
