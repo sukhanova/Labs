7d01ba21af7e4eb4781269bf9f70dce7f442656d
Vira Sukhanova <verusia.s@gmail.com>
2021-04-22 22:42:34 -0500

    Completed arrays.js

--- js-trials-2/arrays.js
+++ js-trials-2/arrays.js
@@ -4,16 +4,32 @@
 // 1. printIndices
 function printIndices(items) {
   // Replace this with your code
+  for (const item in items){
+    console.log (`${items[item]} is at index ${item}`);
+  }
 }
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
   // Replace this with your code
+  const listOfEveryOtherItems = []
+
+  for (const item in items){
+    if (item % 2 === 0){
+      listOfEveryOtherItems.push(items[item])
+    }
+  }
+  console.log(listOfEveryOtherItems)
 }
 
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
   // Replace this with your code
+  const sortedItems = items.sort((a, b) => a - b);
+  sortedItems.slice(0, n);
+  const result = sortedItems.reverse();
+  
+  console.log(result)
 }
