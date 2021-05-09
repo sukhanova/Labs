6901f8de1ce93e8877d5d39c29de1a32e9d87f5b
Vira Sukhanova <verusia.s@gmail.com>
2021-05-08 19:12:30 -0500

    Added AddTradingCard component

--- react-trading-cards-2/static/js/tradingCard.jsx
+++ react-trading-cards-2/static/js/tradingCard.jsx
@@ -49,6 +49,41 @@ const tradingCardData = [
   },
 ];
 
+function AddTradingCard(props) {
+  const [name, setName] = React.useState("");
+  const [skill, setSkill] = React.useState("");
+  function addNewCard() {
+    // TO BE IMPLEMENTED
+    alert('trying to add new card');
+  }
+  return (
+    <React.Fragment>
+      <h2>Add New Trading Card</h2>
+      <label htmlFor="nameInput">Name</label>
+      <input
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        id="nameInput"
+        style={{ marginLeft: "5px" }}
+      ></input>
+      <label
+        htmlFor="skillInput"
+        style={{ marginLeft: "10px", marginRight: "5px" }}
+      >
+        Skill
+      </label>
+      <input
+        value={skill}
+        onChange={(event) => setSkill(event.target.value)}
+        id="skillInput"
+      ></input>
+      <button style={{ marginLeft: "10px" }} onClick={addNewCard}>
+        Add
+      </button>
+    </React.Fragment>
+  );
+}
+
 function TradingCard(props) {
   return (
     <div className="card">
@@ -86,7 +121,14 @@ function TradingCardContainer() {
     .then((data)=>updateCards(data.cards))
   }, [])
 
-  return <div className="grid">{tradingCards}</div>;
+  return (
+    <React.Fragment>
+      <AddTradingCard />
+      <h2>Trading Cards</h2>
+      <div className="grid">{tradingCards}</div>;
+    </React.Fragment>
+  ) 
 }
 
 ReactDOM.render(<TradingCardContainer />, document.getElementById("container"));
+//ReactDOM.render(<AddTradingCard />,document.getElementById('container'));
